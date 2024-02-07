be20cfaf55050e4244de2efb3ee4f7bc7f6a9a57
John Pieri <jvpieri@gmail.com>
2024-02-07 17:41:27 -0400

    Lab Done

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let concatenatedStr = s1.concat(" ", s2 , " " ,s3 ," ", s4, " ", s5)
+
 
 // Print out the concatenated string
 
-
-
+console.log(concatenatedStr)
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -23,23 +24,28 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+let uppercase =  part1[0].toUpperCase() + part1.slice(1) + part2[0].toUpperCase() + part2.slice(1)
+
 
 // Print the cameLtaiL-formatted string
 
+console.log(uppercase)
 
 
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
+
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const totalWithTip = billTotal * (15 / 100)
 
 // Print out the tipAmount
 
-
+console.log(totalWithTip)
 
 
 /*******************************************
@@ -48,9 +54,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+let randomInt =  Math.floor(Math.random() * 10) + 1
 
 // Print the generated random number
 
+console.log(randomInt)
 
 
 /*******************************************
@@ -61,16 +69,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+const expression2 = a || b; // true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
 
-const expression7 = a && a;
+const expression7 = a && a; // true
